2da52ac0fb1836b8b7d426e76c3354a210fcfd33
djenders <dennis@jenders.com>
2023-11-24 09:17:05 -0600

    Update defaults.sh

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -6,6 +6,24 @@ source _utils.sh
 e_pending "Creating defaults"
 # ------------------------------------------------------------------------------
 
+if ! has_path "Developer"; then
+  get_consent "Create ~/Developer folder"
+  if has_consent; then
+    e_pending "Creating ~/Developer folder"
+    mkdir -p ~/Developer
+    test_path "Developer"
+  fi
+fi
+
+if ! has_path "Sandbox"; then
+  get_consent "Create ~/Sandbox folder"
+  if has_consent; then
+    e_pending "Creating ~/Sandbox folder"
+    mkdir -p ~/Sandbox
+    test_path "Sandbox"
+  fi
+fi
+
 if ! has_command "xcode-select"; then
   e_pending "Installing xcode-select (CLI tools)"
   xcode-select --install
@@ -15,6 +33,15 @@ fi
 if ! has_command "brew"; then
   e_pending "Installing brew (Homebrew)"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if has_arm; then
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'>>$HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
   brew doctor
+  brew tap homebrew/cask-fonts
   test_command "brew"
 fi
+
+# ------------------------------------------------------------------------------
+e_message "Defaults complete"
+# ------------------------------------------------------------------------------

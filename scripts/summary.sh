43b4acce88e032949c1d37d701f17e4b06034e9a
djenders <dennis@jenders.com>
2023-11-24 09:20:48 -0600

    Update summary.sh

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -3,19 +3,32 @@
 source _utils.sh
 
 # ------------------------------------------------------------------------------
-e_pending "Creating summary"
+e_message "Creating summary"
 # ------------------------------------------------------------------------------
 
 echo "\r"
 
-e_success "Default commands"
+e_success "$(uname -p) Architecture"
 if has_path "Developer"; then
   e_success "~/Developer"
 else
   e_failure "~/Developer"
 fi
+if has_path "Sandbox"; then
+  e_success "~/Sandbox"
+else
+  e_failure "~/Sandbox"
+fi
+
 test_command "xcode-select"
 test_command "brew"
+
+if has_cask "font-jetbrains-mono-nerd-font"; then
+  e_success "JetBrains Mono Nerd Font"
+else
+  e_failure "JetBrains Mono Nerd Font"
+fi
+
 test_app "Adobe Creative Cloud"
 test_app "Airy"
 test_app "Atom"
@@ -38,8 +51,7 @@ test_app "Spotify"
 test_app "Streamlabs OBS"
 test_app "Cisco Webex Meetings"
 test_app "Zoom"
-e_success "Optimization commands"
 
-echo "\r"
-
-e_settled "Installation complete!"
+# ------------------------------------------------------------------------------
+e_message "Summary complete"
+# ------------------------------------------------------------------------------

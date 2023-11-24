c736731e66df3e619f393c71e2546a3530e5e8eb
djenders <dennis@jenders.com>
2023-11-24 09:24:09 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ e_message "Starting mac-mktg-setup"
 # ------------------------------------------------------------------------------
 
 source defaults.sh
-#source tools.sh
+source tools.sh
 source casks.sh
 source optimizations.sh
 source summary.sh
